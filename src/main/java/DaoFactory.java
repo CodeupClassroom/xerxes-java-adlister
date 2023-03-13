8617b0097ec6ced9771df0842cca9c575724345e
David Stephens <stephens.a.david@gmail.com>
2023-03-13 10:32:50 -0500

    completed jdbc lecture for ymir

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,10 +1,19 @@
 public class DaoFactory {
     private static Ads adsDao;
+    private static Books booksDao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new ListAdsDao();
         }
         return adsDao;
+    }
+
+    public static Books getBooksDao() {
+        if (booksDao == null) {
+            Config config = new Config();
+            booksDao = new MySQLBooksDao(config);
+        }
+        return booksDao;
     }
 }
